ecae5846bde1b8e1d89438a9608cee5b62486ead
adevotus <adevoweb@gmail.com>
2023-04-18 21:44:02 +0300

    register page

--- ifakara_recruitment_system/resources/views/auth/register.blade.php
+++ ifakara_recruitment_system/resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -75,3 +75,161 @@
     </div>
 </div>
 @endsection
+ --}}
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Register | Portal</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../assets/images/favicon.ico">
+
+    <!-- App css -->
+    <link href="{{ asset('assets/css/config/default/bootstrap.min.css') }}" rel="stylesheet" type="text/css"
+        id="bs-default-stylesheet" />
+    <link href="{{ asset('assets/css/config/default/app.min.css') }}" rel="stylesheet" type="text/css"
+        id="app-default-stylesheet" />
+
+    <link href="{{ asset('assets/css/config/default/bootstrap-dark.min.css') }}" rel="stylesheet" type="text/css"
+        id="bs-dark-stylesheet" />
+    <link href="{{ asset('assets/css/config/default/app-dark.min.css') }}" rel="stylesheet" type="text/css"
+        id="app-dark-stylesheet" />
+
+    <!-- icons -->
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body class="loading authentication-bg-pattern">
+
+    <div class="account-pages mt-5 mb-5">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8 col-lg-6 col-xl-4">
+                    <div class="card bg-pattern" style="box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;">
+
+                        <div class="card-body p-4">
+
+                            <div class="text-center w-75 m-auto">
+                                <div class="auth-logo">
+                                    <a href="index.html" class="logo logo-dark text-center">
+                                        <span class="logo-lg">
+                                                {{-- <img src="../assets/images/logo-dark.png" alt="" height="22"> --}}
+                                                <H3 class="text-black">PORTAL | SIGN UP</H3>
+                                            </span>
+                                    </a>
+
+                                    <a href="index.html" class="logo logo-light text-center">
+                                        <span class="logo-lg">
+                                                {{-- <img src="../assets/images/logo-light.png" alt="" height="22"> --}}
+                                                <H3 class="text-black">PORTAL | SIGN UP</H3>
+                                            </span>
+                                    </a>
+                                </div>
+                                {{-- <p class="text-muted mb-4 mt-3">Don't have an account? Create your account, it takes less than a minute</p> --}}
+                            </div>
+
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+
+                                <div class="mb-3">
+                                    <label for="Firstname" class="form-label text-black">{{ __('Name') }}</label>
+                                    <input class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"type="text" id="fullname" placeholder="Enter your name" required>
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                                </div>
+                              
+                                <div class="mb-3">
+                                    <label for="emailaddress" class="form-label text-black">{{ __('Email Address') }}</label>
+                                    <input class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" type="email" id="emailaddress" required placeholder="Enter your email">
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                                </div>
+                                <div class="mb-3">
+                                    <label for="password" class="form-label text-black">{{ __('Password') }}</label>
+                                    <div class="input-group input-group-merge">
+                                        <input type="password" id="password" class="form-control @error('password') is-invalid @enderror" name="password"placeholder="Enter your password">
+                                        <div class="input-group-text" data-password="false">
+                                            <span class="password-eye"></span>
+
+                                        </div>
+                                        @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <label for="password" class="form-label text-black"> {{ __('Confirm Password') }} </label>
+                                    <div class="input-group input-group-merge">
+                                        <input type="password" id="password" class="form-control" name="password_confirmation" required autocomplete="new-password" placeholder="Enter your password">
+                                        <div class="input-group-text" data-password="false">
+                                            <span class="password-eye"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="mb-3">
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-check-input" id="checkbox-signup">
+                                        <label class="form-check-label" for="checkbox-signup">I accept <a href="javascript: void(0);" class="text-dark">Terms and Conditions</a></label>
+                                    </div>
+                                </div>
+                                <div class="text-center d-grid">
+                                    <button class="btn btn-primary" type="submit"> {{ __('Register') }}</button>
+                                </div>
+
+                            </form>
+
+                      
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-12 text-center">
+                                <p class="text-black-50">Already have account? <a href="{{ route('login') }}" class="text-black ms-1"><b>{{ __('Login') }}</b></a></p>
+                            </div>
+                            <!-- end col -->
+                        </div>
+                        <!-- end card-body -->
+                    </div>
+                    <!-- end card -->
+
+                 
+                    <!-- end row -->
+
+                </div>
+                <!-- end col -->
+            </div>
+            <!-- end row -->
+        </div>
+        <!-- end container -->
+    </div>
+    <!-- end page -->
+
+    <footer class="footer footer-alt">
+        
+        <script>
+            document.write(new Date().getFullYear())
+        </script> &copy;  <a href="" class="text-black-50">Ifakara Catholic Diocese</a>
+    </footer>
+
+    <!-- Vendor js -->
+    <script src="{{ asset('assets/js/vendor.min.js') }}"></script>
+
+    <!-- App js -->
+    <script src="{{ asset('assets/js/app.min.js') }}"></script>
+
+</body>
+
+</html>
