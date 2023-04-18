fd866316dfa78400b15167123246e3f950fe7844
adevotus <adevoweb@gmail.com>
2023-04-18 22:20:15 +0300

    user dashboard

---
+++ ifakara_recruitment_system/resources/views/assets/css.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8" />
+    <title>Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="../assets/images/favicon.ico">
+   
+    <!-- App css -->
+    <link href="{{asset('assets/css/config/default/bootstrap.min.css')}}" rel="stylesheet" type="text/css" id="bs-default-stylesheet" />
+    <link href="{{asset('assets/css/config/default/app.min.css')}}" rel="stylesheet" type="text/css" id="app-default-stylesheet" />
+
+    <link href="{{asset('assets/css/config/default/bootstrap-dark.min.css')}}" rel="stylesheet" type="text/css" id="bs-dark-stylesheet" />
+    <link href="{{asset('assets/css/config/default/app-dark.min.css')}}" rel="stylesheet" type="text/css" id="app-dark-stylesheet" />
+
+    <!-- icons -->
+    <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+
+</head>
---
+++ ifakara_recruitment_system/resources/views/assets/js.blade.php
@@ -0,0 +1,11 @@
+    <!-- Vendor js -->
+    <script src="{{asset('/assets/js/vendor.min.js')}}"></script>
+    <!-- Dashboard 2 init -->
+    <script src="{{asset('assets/js/pages/dashboard-2.init.js')}}"></script>
+
+    <!-- App js-->
+    <script src="{{asset('assets/js/app.min.js')}}"></script>
+
+</body>
+
+</html>
