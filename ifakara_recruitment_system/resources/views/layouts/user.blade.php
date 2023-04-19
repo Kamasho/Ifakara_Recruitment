2812f2e096bc415fe601b29340e2c4c63d3a4751
Kamasho Godfrey <kamashogodfrey997@gmail.com>
2023-04-19 23:15:39 +0300

    last commit tar 19

--- ifakara_recruitment_system/resources/views/layouts/user.blade.php
+++ ifakara_recruitment_system/resources/views/layouts/user.blade.php
@@ -6,7 +6,7 @@
     <div class="wrapper">
         <div class="section">
             <div class="top_navbar">
-
+                <p>Ifakara Dioceses Recruitment Portal</p>
             </div>
             <div class="top_navbar_hamburger">
                 <div class="hamburger">
@@ -14,6 +14,17 @@
                         <i class="fas fa-bars"></i>
                     </a>
                 </div>
+                <div class="items">
+                    <a href="" class="button">home</a>
+                    <a href="" class="button">Notifications</a>
+                    <a href="" class="button">Vacancies</a>
+                    <a href="" class="button">My Applications</a>
+                    <a href="" class="button">Register To Job Alerts</a>
+                    <a href="" class="button">Change Password</a>
+                    <a href="" class="button">Logout</a>
+                    <a href="" class="button">Feedback</a>
+                </div>
+                
             </div>
             <div class="main">
                 <div class="container-fluid">
