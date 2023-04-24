c79d3a2aa69f39da42dddeac49bd91002263ae2f
Kamasho Godfrey <kamashogodfrey997@gmail.com>
2023-04-24 14:38:12 +0300

    user route updated

--- ifakara_recruitment_system/routes/web.php
+++ ifakara_recruitment_system/routes/web.php
@@ -4,9 +4,17 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GS\GsController;
 use App\Http\Controllers\HR\HrController;
+use App\Http\Controllers\User\AcademicQualificationController;
+use App\Http\Controllers\User\ComputerLiterancyController;
+use App\Http\Controllers\User\LanguageProficiencyController;
+use App\Http\Controllers\User\OnlineCourseController;
+use App\Http\Controllers\User\OtherAttachimentController;
 use App\Http\Controllers\User\PersonalDetailsController;
+use App\Http\Controllers\User\ProfessionalQualificationController;
+use App\Http\Controllers\User\RefereesController;
+use App\Http\Controllers\User\TrainingAndWorkshopController;
 use App\Http\Controllers\User\UserController;
-
+use App\Http\Controllers\User\WorkingExperienceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -39,8 +47,66 @@ Route::middleware(['auth','isGs'])->group(function(){
 
  //Users middleware
 Route::middleware(['auth','isUser'])->group(function(){
+    //Dashboard
     Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
 
     //Personal Details
     Route::get('/personaldetails', [PersonalDetailsController::class, 'index'])->name('personaldetails');
- });
+    Route::post('/personaldetails/add_personaldetails',[PersonalDetailsController::class, 'store'])->name('add_personaldetails');
+    Route::get('/personaldetails/delete_personaldetails/{id}', [PersonalDetailsController::class, 'destroy'])->name('delete_personaldetails');
+    Route::put('/personaldetails/update_personaldetails/{id}', [PersonalDetailsController::class, 'update'])->name('update_personaldetails');
+
+    //Academic Qualifications
+    Route::get('/academicqualifications', [AcademicQualificationController::class, 'index'])->name('academicqualifications');
+    Route::post('/academicqualifications/add_academicqualifications',[AcademicQualificationController::class, 'store'])->name('add_academicqualifications');
+    Route::get('/academicqualifications/delete_academicqualifications/{id}', [AcademicQualificationController::class, 'destroy'])->name('delete_academicqualifications');
+    Route::put('/academicqualifications/update_academicqualifications/{id}', [AcademicQualificationController::class, 'update'])->name('update_academicqualifications');
+
+    //Computer Literancy
+    Route::get('/computerliterancy', [ComputerLiterancyController::class, 'index'])->name('computerliterancy');
+    Route::post('/computerliterancy/add_computerliterancy',[ComputerLiterancyController::class, 'store'])->name('add_computerliterancy');
+    Route::get('/computerliterancy/delete_computerliterancy/{id}', [ComputerLiterancyController::class, 'destroy'])->name('delete_computerliterancy');
+    Route::put('/computerliterancy/update_computerliterancy/{id}', [ComputerLiterancyController::class, 'update'])->name('update_computerliterancy');
+
+    //Language Proficiency
+    Route::get('/languageproficiency', [LanguageProficiencyController::class, 'index'])->name('languageproficiency');
+    Route::post('/languageproficiency/add_languageproficiency',[LanguageProficiencyController::class, 'store'])->name('add_languageproficiency');
+    Route::get('/languageproficiency/delete_languageproficiency/{id}', [LanguageProficiencyController::class, 'destroy'])->name('delete_languageproficiency');
+    Route::put('/languageproficiency/update_languageproficiency/{id}', [LanguageProficiencyController::class, 'update'])->name('update_languageproficiency');
+
+    //Online Course
+    Route::get('/onlinecourse', [OnlineCourseController::class, 'index'])->name('onlinecourse');
+    Route::post('/onlinecourse/add_onlinecourse',[OnlineCourseController::class, 'store'])->name('add_onlinecourse');
+    Route::get('/onlinecourse/delete_onlinecourse/{id}', [OnlineCourseController::class, 'destroy'])->name('delete_onlinecourse');
+    Route::put('/onlinecourse/update_onlinecourse/{id}', [OnlineCourseController::class, 'update'])->name('update_onlinecourse');
+
+    //Other Attachiments
+    Route::get('/otherattachiment', [OtherAttachimentController::class, 'index'])->name('otherattachiment');
+    Route::post('/otherattachiment/add_otherattachiment',[OtherAttachimentController::class, 'store'])->name('add_otherattachiment');
+    Route::get('/otherattachiment/delete_otherattachiment/{id}', [OtherAttachimentController::class, 'destroy'])->name('delete_otherattachiment');
+    Route::put('/otherattachiment/update_otherattachiment/{id}', [OtherAttachimentController::class, 'update'])->name('update_otherattachiment');
+
+    //Professional Qualification
+    Route::get('/professionalqualification', [ProfessionalQualificationController::class, 'index'])->name('professionalqualification');
+    Route::post('/professionalqualification/add_professionalqualification',[ProfessionalQualificationController::class, 'store'])->name('add_professionalqualification');
+    Route::get('/professionalqualification/delete_professionalqualification/{id}', [ProfessionalQualificationController::class, 'destroy'])->name('delete_professionalqualification');
+    Route::put('/professionalqualification/update_professionalqualification/{id}', [ProfessionalQualificationController::class, 'update'])->name('update_professionalqualification');
+
+    //Referees
+    Route::get('/referees', [RefereesController::class, 'index'])->name('referees');
+    Route::post('/referees/add_referees',[RefereesController::class, 'store'])->name('add_referees');
+    Route::get('/referees/delete_referees/{id}', [RefereesController::class, 'destroy'])->name('delete_referees');
+    Route::put('/referees/update_referees/{id}', [RefereesController::class, 'update'])->name('update_referees');
+
+    //Training And Workshop
+    Route::get('/trainingandworkshop', [TrainingAndWorkshopController::class, 'index'])->name('trainingandworkshop');
+    Route::post('/trainingandworkshop/add_trainingandworkshop',[TrainingAndWorkshopController::class, 'store'])->name('add_trainingandworkshop');
+    Route::get('/trainingandworkshop/delete_trainingandworkshop/{id}', [TrainingAndWorkshopController::class, 'destroy'])->name('delete_trainingandworkshop');
+    Route::put('/trainingandworkshop/update_trainingandworkshop/{id}', [TrainingAndWorkshopController::class, 'update'])->name('update_trainingandworkshop');
+
+    //Working Experiences
+    Route::get('/workingexperience', [WorkingExperienceController::class, 'index'])->name('workingexperience');
+    Route::post('/workingexperience/add_workingexperience',[WorkingExperienceController::class, 'store'])->name('add_workingexperience');
+    Route::get('/workingexperience/delete_workingexperience/{id}', [WorkingExperienceController::class, 'destroy'])->name('delete_workingexperience');
+    Route::put('/workingexperience/update_workingexperience/{id}', [WorkingExperienceController::class, 'update'])->name('update_workingexperience');
+});
