c3a6092da43ed138599d0fa98848daeb7fb24545
Kamasho Godfrey <kamashogodfrey997@gmail.com>
2023-04-24 12:05:57 +0300

    user model design

---
+++ ifakara_recruitment_system/app/Models/TrainingAndWorkshop.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TrainingAndWorkshop extends Model
+{
+    use HasFactory;
+}
