97e71904592691acf7b6a7c800af4b3e06741e95
Kamasho Godfrey <kamashogodfrey997@gmail.com>
2023-04-25 08:46:08 +0300

    some backend update

--- ifakara_recruitment_system/resources/views/users/personaldetails.blade.php
+++ ifakara_recruitment_system/resources/views/users/personaldetails.blade.php
@@ -9,7 +9,6 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box">
-
                         <h4 class="page-title">Person Details</h4>
                     </div>
                 </div>
